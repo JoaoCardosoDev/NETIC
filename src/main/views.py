e04bc0ddc6e85ed1adea8359f98030625959f95a
João Cardoso <136370136+JoaoCardosoDev@users.noreply.github.com>
2024-11-03 18:35:36 +0000

    Django form handling & model creation for testing

--- src/main/views.py
+++ src/main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import ProfileForm
 from .context import context
 
 def netics_home (request) :
@@ -6,5 +8,12 @@ def netics_home (request) :
     return render (request, "mainPage/index.html", context)
 
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        context["form"] = ProfileForm()
 
     return render (request, "profilePage/index.html", context)
